8ce0d844d0ba016f5a96bbda99e1c59ba78a92d5
quaoz <74419801+quaoz@users.noreply.github.com>
2022-06-25 22:20:58 +0000

    Update powercord_update.sh

--- powercord_update.sh
+++ powercord_update.sh
@@ -4,7 +4,7 @@ echo 'Updating powercord & plugins...'
 echo
 
 cd $(pwd)/src/Powercord/plugins
-find . -maxdepth 1 -type d \( ! -name "pc-*" \) -exec bash -c "git pull" \;
+find . -maxdepth 1 -type d \( ! -name "pc-*" \) -exec bash -c "cd '{}' && git pull" \;
 
 echo
 echo 'Updating themes...'
@@ -12,7 +12,7 @@ echo
 
 cd ../themes
 
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "git pull" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && git pull" \;
 
 echo
 echo 'All done'
