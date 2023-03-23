5847821b459476985feeab1fd31d056c5bdc04f7
quaoz <74419801+quaoz@users.noreply.github.com>
2023-03-23 18:10:30 +0000

    Delete transmission_blocklists.sh

--- transmission_blocklists.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s https://www.iblocklist.com/lists.php \
- | sed -n "s/.*value='\(http:.*\)'.*/\1/p" \
- | xargs wget -O - \
- | gunzip \
- | egrep -v '^#' > blocklists/blocklist.txt
